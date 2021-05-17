6fa3746fdf2bd9b830453f075ba76c80b5abb332
Parker Ackerman <trashcastermage@gmail.com>
2021-05-17 10:02:34 -0400

    Removed more configs

--- src/main/java/iskallia/vault/init/ModConfigs.java
+++ src/main/java/iskallia/vault/init/ModConfigs.java
@@ -10,9 +10,7 @@ public class ModConfigs {
     public static SkillDescriptionsConfig SKILL_DESCRIPTIONS;
     public static SkillGatesConfig SKILL_GATES;
     public static VaultLevelsConfig LEVELS_META;
-    public static VaultItemsConfig VAULT_ITEMS;
     public static OverLevelEnchantConfig OVERLEVEL_ENCHANT;
-    public static PlayerExpConfig PLAYER_EXP;
 
     public static void register() {
         RESEARCHES = (ResearchConfig) new ResearchConfig().readConfig();
@@ -20,9 +18,7 @@ public class ModConfigs {
         SKILL_DESCRIPTIONS = (SkillDescriptionsConfig) new SkillDescriptionsConfig().readConfig();
         SKILL_GATES = (SkillGatesConfig) new SkillGatesConfig().readConfig();
         LEVELS_META = (VaultLevelsConfig) new VaultLevelsConfig().readConfig();
-        VAULT_ITEMS = (VaultItemsConfig) new VaultItemsConfig().readConfig();
         OVERLEVEL_ENCHANT = (OverLevelEnchantConfig) new OverLevelEnchantConfig().readConfig();
-        PLAYER_EXP = (PlayerExpConfig) new PlayerExpConfig().readConfig();
         Vault.LOGGER.info("Vault Configs are loaded successfully!");
     }
 
